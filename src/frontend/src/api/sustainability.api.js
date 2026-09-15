0af6539a8b60f6ebb9390f604d30f84ef05fa3fa
Pratham2022 <pratham912022@gmail.com>
2026-09-15 19:41:23 +0530

    Revert "Final Version"

--- src/frontend/src/api/sustainability.api.js
+++
@@ -1,6 +0,0 @@
-import apiClient from './client';
-
-export const sustainabilityApi = {
-  getScore: () => apiClient.get('/farmer/sustainability'),
-  updateParameters: (data) => apiClient.post('/farmer/sustainability/update', data),
-};
--- src/frontend/src/api/weather.api.js
+++
@@ -1,5 +0,0 @@
-import apiClient from './client';
-
-export const weatherApi = {
-  getWeather: (location = 'Ahmedabad') => apiClient.get(`/farmer/weather?location=${encodeURIComponent(location)}`),
-};
